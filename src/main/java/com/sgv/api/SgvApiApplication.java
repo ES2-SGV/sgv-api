938adbcf83b286814568dd6ad4a1ce0d188a3cfb
ugoincc <hugo.incc@hotmail.com>
2026-08-18 10:41:37 -0300

    Spring Boot setup

---
+++ src/main/java/com/sgv/api/SgvApiApplication.java
@@ -0,0 +1,13 @@
+package com.sgv.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgvApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgv/api/SgvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgv.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
